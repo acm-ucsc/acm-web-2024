2696decbb51787b2e3b01e308d9f506aea259005
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-07-22 19:44:46 +0530

    Styles fix in hero section

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,24 +1,29 @@
 import logo from "../../../public/assets/images/acm-logo-white.png";
 import Image from "next/image";
 const navLinks = [
-    { href: "/", label: "Home" },
-    { href: "/about", label: "About" },
-    { href: "/events", label: "Events" },
-    { href: "/contact", label: "Contact" },
-  ];
-  
-  export default function Navbar() {
-    return (
-      <div className="fixed top-0 left-0 right-0 z-50 flex flex-row items-center justify-between p-4 shadow-lg h-[60px]">
-        <div className="text-xl font-bold"><Image src={logo} alt="ACM UCSC" height={100} width={100} ></Image></div>
-        <nav className="flex space-x-4">
-          {navLinks.map((link) => (
-            <a key={link.href} href={link.href} className="text-white hover:text-secondary transition duration-300">
-              <b>{link.label}</b>
-            </a>
-          ))}
-        </nav>
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/events", label: "Events" },
+  { href: "/contact", label: "Contact" },
+];
+
+export default function Navbar() {
+  return (
+    <div className="fixed top-0 left-0 right-0 z-50 flex flex-row items-center justify-between p-4 shadow-lg h-[60px] bg-black ">
+      <div className="text-xl font-bold">
+        <Image src={logo} alt="ACM UCSC" height={100} width={100}></Image>
       </div>
-    );
-  }
-  
+      <nav className="flex space-x-4">
+        {navLinks.map((link) => (
+          <a
+            key={link.href}
+            href={link.href}
+            className="text-white hover:text-secondary transition duration-300"
+          >
+            <b>{link.label}</b>
+          </a>
+        ))}
+      </nav>
+    </div>
+  );
+}
