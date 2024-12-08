573cb7e1cbc185f47fc3b86f7509e8a176f8b30a
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-12-08 11:23:59 +0530

    Tried to fix the dynamic path for the images

--- src/components/timeline/carousel.tsx
+++ src/components/timeline/carousel.tsx
@@ -4,46 +4,50 @@ import { useInView } from "react-intersection-observer";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
+import NOI_Image from "./NOI_2024/NOI_2024.jpg";
+import SIGCHI_Image from "./SIGCHI_2024/sigchi_2024.jpg";
+import ReidXtreme_Image from "./ReidXtream_2024/reid_xtreme2024.jpg";
+import Linux_Kickstart_Image from "./Monthly_Meetup_01-Linux_Kickstart/linux_kick_start.jpg";
+import GitHub_Azure_Image from "./Monthly_Meetup_02-GitHub_and_Azure_Cloud/monthly_meetup_github_azure.jpg";
+
+
 const events = [
   {
     title: "NOI 2024",
     description:
       "NOI 2024 is the National Olympiad in Informatics, a prestigious programming competition for high school students in Sri Lanka.",
     date: "2024",
-    imageUrl: "/assets/images/events/NOI_2024/NOI_2024.JPG",
+    imageUrl: NOI_Image,
   },
   {
     title: "SIGCHI 2024",
     description:
       "Winter School by Colombo ACM SIGCHI Chapter: Connecting South Asia to collaborate and develop research interest in HCI.",
     date: "2nd, 3rd, and 4th January",
-    imageUrl: "/assets/images/events/SIGCHI_2024/sigchi_2024.jpg",
+    imageUrl: SIGCHI_Image,
   },
   {
     title: "ReidXtream 2024",
     description:
       "ReidXtreme 3.0 is the premier competitive programming hackathon organized by the IEEE Student Branch of UCSC in collaboration with the ACM Student Chapter of UCSC.",
     date: "7th September",
-    imageUrl: "/assets/images/events/ReidXtream_2024/reid_xtreme2024.jpg",
+    imageUrl: ReidXtreme_Image,
   },
   {
     title: "Linux Kickstart: Hands-On Basics - Monthly Meetup June",
     description:
       "Linux Kickstart: Hands-On Basics - Monthly Meetup June is a hands-on workshop to learn the basics of Linux.",
     date: "29th and 30th June",
-    imageUrl:
-      "/assets/images/events/Monthly_Meetup_01-Linux_Kickstart/linux_kick_start.jpg",
+    imageUrl: Linux_Kickstart_Image,
   },
   {
     title: "Advanced GitHub and Azure Cloud - Monthly Meetup July",
     description:
       "Advanced GitHub and Azure Cloud - Monthly Meetup July is a hands-on workshop to learn advanced GitHub and Azure Cloud.",
     date: "25th July",
-    imageUrl:
-      "/assets/images/events/Monthly_Meetup_02-GitHub_and_Azure_Cloud/monthly_meetup_github_azure.jpg",
+    imageUrl: GitHub_Azure_Image,
   },
 ];
-
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [itemsPerView, setItemsPerView] = useState(4); // Default for larger screens
@@ -117,10 +121,10 @@ const Carousel = () => {
                   <h5 className="text-xl text-white font-semibold text-center">
                     {event.title}
                   </h5>
-                  <p className="text-gray-500 text-1xl mt-2 flex-1 text-center">
+                  <p className="text-gray-500 h-auto text-1xl mt-2 flex-1 text-center">
                     {event.description}
                   </p>
-                  <p className="text-sm text-gray-500 mt-4 text-center">
+                  <p className="text-sm text-white mt-4 text-center">
                     {event.date}
                   </p>
                 </div>
