d0941662a65922f10102db0f6ed69b6517823d63
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2025-01-13 12:24:13 +0530

    out removed next.config.mjs modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // distDir: "dist",
+  // images: {
+  //   unoptimized: true,
+  // },
   trailingSlash: true,
 };
 
